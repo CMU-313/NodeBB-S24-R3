6d74d701cd9e229268b3f4eca4c20c6db7839af1
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2024-02-05 13:17:21 -0500

    added suggested username during registering process

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,8 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    const suggested_username = username + "suffix";
+                    showError(username_notify, `[[error:username-taken]]. Maybe try "${suggested_username}"`);
                 }
 
                 callback();
