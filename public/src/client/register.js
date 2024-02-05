f3a391900829e623e0c7ebee9b2737af44463fe5
Luke <lukeblackwell@protonmail.com>
2024-02-05 15:26:21 -0500

    Added sugestions for email

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,10 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    // show error if username is taken and suggest a new one with suffix
+                    showError(username_notify, '[[error:username-taken]]' + "${username}suffix");
+                    // showError(username_notify, '[[error:username-taken]]');
+                    sho
                 }
 
                 callback();
