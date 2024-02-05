dc5cda954c9e55d505b1b45558dfd616dee822dd
Andy Zhu <51041969+andyxzhu@users.noreply.github.com>
2024-02-05 06:45:47 -0800

    update username taken error with double quotes

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -132,7 +132,7 @@ define('forum/register', [
                     showSuccess(username_notify, successIcon);
                 } else {
 
-                    showError(username_notify, '[[error:username-taken, ' + username + 'suffix' + ']]');
+                    showError(username_notify, '[[error:username-taken, ' + '\"' + username + 'suffix' + '\"' + ']]');
                 }
 
                 callback();
