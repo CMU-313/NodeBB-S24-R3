91bfed7e423ab47c55f4010b5e61782df6cb2299
David Wang <dmwsg68@gmail.com>
2024-02-05 09:37:04 -0500

    added username suggestions if a username is taken

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,7 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    usernameError(username_notify, '[[error:username-taken]]', username);
                 }
 
                 callback();
@@ -177,6 +177,18 @@ define('forum/register', [
 
     function showError(element, msg) {
         translator.translate(msg, function (msg) {
+            element.html(msg);
+            element.parent()
+                .removeClass('register-success')
+                .addClass('register-danger');
+            element.show();
+        });
+        validationError = true;
+    }
+
+    function usernameError(element, msg, username) {
+        translator.translate(msg, function (msg) {
+            msg = msg.concat(`. Maybe try ${username}suffix`);
             element.html(msg);
             element.parent()
                 .removeClass('register-success')
