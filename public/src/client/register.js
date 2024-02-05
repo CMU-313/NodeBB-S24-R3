50e3d17ccbfe67c19b1d292e2bd7e2b254001481
Anna Zhang <annazhan@andrew.cmu.edu>
2024-02-05 13:07:52 -0500

    Added suggestion for alternate username when already taken

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,8 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    // showError(username_notify, '[[error:username-taken]]');
+                    showError(username_notify, `[[error:username-taken, ${username}]]`);
                 }
 
                 callback();
