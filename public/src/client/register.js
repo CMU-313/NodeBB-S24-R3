8eac05481160d1988765a755eaf9720adb1957cb
csuraparaju <csuraparaju@wpi.edu>
2024-02-05 09:45:39 -0500

    better workaround

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,8 +131,7 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    // showError(username_notify, '[[error:username-taken]]');
-                    showError(username_notify, `Username is already taken. Try ${username}suffix instead.`);
+                    showError(username_notify, `[[error:username-taken, ${username}suffix]]`);
                 }
 
                 callback();
