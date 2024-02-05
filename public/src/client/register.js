41ee0b2442002a8e6dc0ac71ffb8d246f85cc6ef
Joyce Hong <joyce.hong8@gmail.com>
2024-02-04 20:06:38 -0500

    change from random number to 'suffix' string

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,7 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, `[[error:username-taken]] Try ${username}${Math.floor(Math.random() * 10000)}`);
+                    showError(username_notify, `[[error:username-taken]]. Try ${username}suffix`);
                 }
 
                 callback();
