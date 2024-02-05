bb38ce5831ee04880a251b60ab3523791e0f7c47
edwincha <edwincha@DESKTOP-UUF0QSD>
2024-02-05 10:48:39 -0500

    Finished reci

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,7 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken, ' + username + 'suffix' ,']]');
+                    showError(username_notify, '[[error:username-taken, "${username}suffix"]]');
                 }
 
                 callback();
